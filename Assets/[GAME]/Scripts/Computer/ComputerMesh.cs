14e6638f6a797b011496c4b695a5f53b61f078d4
Çağlar Çınkır <60190180+caglar7@users.noreply.github.com>
2022-10-24 17:44:34 +0300

    update, working on updating computer mesh

---
+++ Assets/[GAME]/Scripts/Computer/ComputerMesh.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComputerMesh : MonoBehaviour
+{
+    [SerializeField] Transform[] deactivateMeshes;
+
+    private void Start()
+    {
+        foreach(Transform t in deactivateMeshes)
+        {
+            t.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/[GAME]/Scripts/Computer/ComputerMeshControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComputerMeshControl : MonoBehaviour
+{
+    public Transform body_Nothing;
+    public Transform body_KeyboardFrame;
+    public Transform body_MousepadFrame;
+    public Transform body_KeyboardFrame_MousepadFrame;
+    public Transform body_Keyboard;
+    public Transform screen_Frame;
+    public Transform screen_Filled;
+}
+
+//// this gonna change when there are blendshapes
+//public enum ComputerMeshType
+//{
+//    Body_Nothing,
+//    Body_KeyboardFrame,
+//    Body_MousepadFrame,
+//    Body_KeyboardFrame_MousepadFrame,
+//    Screen,
+//}
