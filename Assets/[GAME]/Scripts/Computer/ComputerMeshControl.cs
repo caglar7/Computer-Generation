e4dda25818732b1ba8d70f6fd3f7f8a53147b17f
Çağlar Çınkır <60190180+caglar7@users.noreply.github.com>
2022-10-24 17:53:40 +0300

    update

--- Assets/[GAME]/Scripts/Computer/ComputerMeshControl.cs
+++ Assets/[GAME]/Scripts/Computer/ComputerMeshControl.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class ComputerMeshControl : MonoBehaviour
 {
+    // follow what meshes object has
+    // depending on what components we got
+    // => body, keyboard frame, mousepad frame, keyboard, mousepad, screen frame, screen
+
     public Transform body_Nothing;
     public Transform body_KeyboardFrame;
     public Transform body_MousepadFrame;
@@ -11,6 +15,18 @@ public class ComputerMeshControl : MonoBehaviour
     public Transform body_Keyboard;
     public Transform screen_Frame;
     public Transform screen_Filled;
+
+    // IMPLEMENT
+    // methods like this are gonna be used by the robots
+    public void AddKeyboardFrame()
+    {
+
+    }
+
+    public void AddMousepadFrame()
+    {
+
+    }
 }
 
 //// this gonna change when there are blendshapes
