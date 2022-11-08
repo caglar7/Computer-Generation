544f2602a8d05b71a77036627e07a160a225a058
Çağlar Çınkır <60190180+caglar7@users.noreply.github.com>
2022-11-08 17:53:01 +0300

    total money UI singleton access object

---
+++ Assets/[GAME]/Scripts/AccessObjects/TotalMoneyUI.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TotalMoneyUI : MonoBehaviour
+{
+    #region Singleton
+    public static TotalMoneyUI instance;
+    private void Awake()
+    {
+        if (instance == null) instance = this;
+    } 
+    #endregion
+
+    public TextMeshProUGUI text;
+    public int revenue = 0;
+    
+    public void UpdateRevenue(int added = 0)
+    {
+        // value
+        revenue = PlayerPrefs.GetInt("Revenue", 0);
+        revenue += added;
+        PlayerPrefs.SetInt("Revenue", revenue);
+
+        // UI
+        text.text = revenue.ToString() + "$";
+    }
+}
