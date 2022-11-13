ff07c1402d61c8b517f7f03e6771883ede332f94
Çağlar Çınkır <60190180+caglar7@users.noreply.github.com>
2022-11-13 21:22:16 +0300

    update, working on level end

---
+++ Assets/[GAME]/Scripts/LevelEnd/LevelEndBoxes.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class LevelEndBoxes : MonoBehaviour
+{
+    [Header("Generate Settings")]
+    [SerializeField] Transform startPos;
+    [SerializeField] float offsetY = 6f;
+    [SerializeField] int howMany = 20;
+    Vector3 nextLocal;
+
+    [Header("Box Objects")]
+    [SerializeField] GameObject[] boxes;
+
+    [Header("Text Settings")]
+    [SerializeField] int startMoney = 200;
+    [SerializeField] int increase = 200;
+    int currentMoney = 200;
+
+    private void Start()
+    {
+        nextLocal = startPos.localPosition;
+        currentMoney = startMoney;
+        GenerateBoxes();
+    }
+
+    void GenerateBoxes()
+    {
+        for (int i = 0; i < howMany; i++)
+        {
+            // get obj, set pos
+            int index = i % 2;
+            GameObject copy = Instantiate(boxes[index], transform);
+            copy.transform.localPosition = nextLocal;
+            nextLocal.y += offsetY;
+
+            // set text
+            copy.GetComponentInChildren<TextMeshPro>().text = Utils.instance.ConvertedNumber(currentMoney);
+            currentMoney += increase;
+        }
+    }
+}
